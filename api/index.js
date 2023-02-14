ba4dea067108438d6dbb3384b5d557168a35c40c
Cristian Noreña <cristian.rraga95@hotmail.com>
2023-02-14 15:05:48 -0500

    Gestión avanzada de errores: Throw

--- api/index.js
+++ api/index.js
@@ -5,17 +5,17 @@ const config = require('../config.js');
 const auth = require('./components/auth/auth.network');
 const user = require('./components/user/user.network');
 const swaggerDoc = require('./swagger.json');
-
+const errors = require('../network/errors');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
-
 app.use('/api/user', user);
 app.use('/api/auth', auth);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use(errors);
 
 app.listen(config.api.port, () => {
   console.log(`Api escuchando en el puerto ${config.api.port}`)
